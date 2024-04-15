4aebca6e2b85925247b050921bea4f49e5618527
wylited <44154103+wylited@users.noreply.github.com>
2024-04-15 18:03:16 +0800

    cleanup

--- backend/src/books.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Book {
-    isbn: String,
-    name: String,
-    authors: Vec<String>,
-    tags: Vec<String>,
-}
