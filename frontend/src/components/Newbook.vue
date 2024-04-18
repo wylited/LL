f80ae43b0a0d97801a85ea76fb388e0f6292289d
Alex Climie <55697723+13carpileup@users.noreply.github.com>
2024-04-18 09:10:20 +0800

    unfunctional posting
    literally doesn't work

--- frontend/src/components/Newbook.vue
+++ frontend/src/components/Newbook.vue
@@ -1,12 +1,18 @@
 
 <template>
     <div class = "menu">
-        <p>NEW</p>
+        <h2 class = "tit">Create a new book repository!</h2>
+        <p class = "sub">If your textbook isn't already on the website, don't worry - you can put it on.</p>
+        <input placeholder = "Book Title" class = "inp" type="bookTitle" v-model="bookTitle" />
+        <input placeholder = "ISBN Number" class = "inp" type="bookISBN" v-model="bookISBN" />
+        <input placeholder = "Front Cover Image URL" class = "inp" type="bookimgURL" v-model="bookimgURL" />
+        <input placeholder = "Author" class = "inp" type="bookAuthor" v-model="bookAuthor" />
         <p>Book Title is: {{ bookTitle }}</p>
-        <p>Message is: {{ email }}</p>
-        <p>Message is: {{ email }}</p>
-        <input class = "inp" type="bookTitle" v-model="bookTitle" />
-        <input class = "inp" type="email" v-model="email" />
+        <p>ISBN is: {{ bookISBN }}</p>
+        <p>img url is: {{ bookimgURL }}</p>
+        <img :src = bookimgURL alt = "Image not loaded">
+        <p>author is: {{ bookAuthor }}</p>
+        
     
         <button @click="submit">Submit</button>
     </div>
@@ -14,8 +20,15 @@
   
 <style>
 
+
+
+  .sub {
+    font-style: italic;
+  }
+
   .menu {
     display: block;
+    margin-left:4vw;
   }
 
   .inp {
@@ -25,23 +38,28 @@
 </style>
 
   <script>
-    
-import { ref } from 'vue'
+    import axios, {isCancel, AxiosError} from 'axios';
+    import { ref } from 'vue'
 
-const message = ref('')
+    const message = ref('')
 
-  export default {
-    data() {
-      return {
-        email: '',
-        bookTitle: ''
-      }
-    },
-    methods: {
-      submit() {
-        console.log(this.email)
-      }
+    export default {
+        data() {
+        return {
+            bookISBN: '',
+            bookTitle: '',
+            bookimgURL:'',
+            bookAuthor:''
+        }
+        },
+        methods: {
+        submit() {
+            console.log(this.bookISBN)
+            axios.post('http://188.166.250.75:3000/api/books',{ params: { isbn: parseInt(this.bookISBN), title: this.bookTitle, authors: [this.bookAuthor], image_url: this.bookimgURL }})
+            .then(response => console.log('actually posted'))
+            console.log('posted fr (i hope)')
+        }
+        }
     }
-  }
   </script>
   
