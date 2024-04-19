0cca5ed850dcd5e734606894e913a5bda4fee629
wylited <44154103+wylited@users.noreply.github.com>
2024-04-19 22:36:27 +0800

    update for compile

--- Summer Physics Model/Assets/ImageTracker.cs
+++ Summer Physics Model/Assets/ImageTracker.cs
@@ -2,20 +2,111 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.Networking;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class ImageTracker : MonoBehaviour
 {
+    [System.Serializable]
+    public class ResourcesData
+    {
+        public List<ResourceData> resources;
+    }
+
+    [System.Serializable]
+    public class ResourceData
+    {
+        public string id;
+        public long book_isbn;
+        public string title;
+        public string author;
+        public string description;
+        public string file_name;
+        public int collab_score;
+        public int page_number;
+    }
+
     private ARTrackedImageManager trackedImages;
     public GameObject[] ArPrefabs;
+    private Renderer[] planeRenderers;
 
     List<GameObject> ARObjects = new List<GameObject>();
 
+    private IEnumerator DownloadandLoadTextures() {
+        Debug.Log("getting textures");
+        yield return DownloadAllTextureAsset((textures) => {
+            Debug.Log("Got Textures");
+            if (textures != null)
+            {
+                foreach (var texturePair in textures)
+                {
+                    var pageNumber = "page-0" + texturePair.Key;
+                    var texture = texturePair.Value;
+                    var ratio = texture.width / texture.height;
 
-    void Awake()
+                    if (texture != null && 14 <= texturePair.Key && 22 >= texturePair.Key ) {
+                        Renderer planeRenderer = planeRenderers[texturePair.Key - 12];
+                        planeRenderer.sharedMaterial.mainTexture = texture;
+                        Vector3 scale = new Vector3(0.3f * ratio, 0.3f, 0.3f);
+                        planeRenderer.transform.localScale = scale;
+                    }
+                    else {
+                        Debug.LogError($"Failed to download texture for page {pageNumber}.");
+                    }
+                }
+                Debug.Log("Finished going through textures");
+            }
+            else
+            {
+                Debug.LogError("Failed to download textures.");
+            }
+        });
+
+    }
+
+
+    public static IEnumerator DownloadAllTextureAsset(System.Action<Dictionary<int, Texture>> callback)
     {
+        using (var request = UnityWebRequest.Get($"http://188.166.250.75:3000/api/resources/9781009071888/unity"))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                var textures = new Dictionary<int, Texture>();
+                var resources = JsonUtility.FromJson<ResourcesData>(request.downloadHandler.text);
+                foreach (var resource in resources.resources)
+                {
+                    using (var req2 = UnityWebRequestTexture.GetTexture($"http://188.166.250.75:3000/api/resources/9781009071888/{resource.id}/image"))
+                    {
+                        yield return req2.SendWebRequest();
+
+                        if (req2.result == UnityWebRequest.Result.Success)
+                        {
+                            textures[resource.page_number] = ((DownloadHandlerTexture)req2.downloadHandler).texture;
+                        }
+                        else
+                        {
+                            Debug.Log($"Error downloading texture: {req2.error}");
+                        }
+                    }
+                }
+                callback(textures);
+            }
+            else
+            {
+                Debug.LogError($"Error downloading resources: {request.error}");
+            }
+        }
+    }
+
+    void Awake() {
+        planeRenderers = new Renderer[ArPrefabs.Length];
+        for (int i = 0; i < ArPrefabs.Length; i++) {
+            planeRenderers[i] = ArPrefabs[i].GetComponent<Renderer>();
+        }
         trackedImages = GetComponent<ARTrackedImageManager>();
+        StartCoroutine(DownloadandLoadTextures());
     }
 
     void OnEnable()
